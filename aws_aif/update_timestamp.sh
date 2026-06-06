aee814415f2d404167111a9877dc4b9c20aa59b2
Neha Singhal <nehasinghal@users.noreply.github.com>
2026-06-05 21:16:39 -0700

    fix: remove incomplete duplicate Q182 (kept full Q513)

--- aws_aif/update_timestamp.sh
+++ aws_aif/update_timestamp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Run before commit: bash update_timestamp.sh && git add AWS_AIF_C01_Learning_Tool.html
 TS=$(date "+%-m/%-d/%Y, %-I:%M:%S %p")
 sed -i '' "s|Updated: .*'|Updated: $TS'|" AWS_AIF_C01_Learning_Tool.html
