b26f820540b6b257e208908b4e9dd821c4867b48
Neha Singhal <nehasinghal@users.noreply.github.com>
2026-06-05 21:15:02 -0700

    fix: use current time for auto-timestamp

--- aws_aif/update_timestamp.sh
+++ aws_aif/update_timestamp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-TS=$(date -r "$(git log -1 --format='%ct' -- AWS_AIF_C01_Learning_Tool.html)" "+%-m/%-d/%Y, %-I:%M:%S %p")
+TS=$(date "+%-m/%-d/%Y, %-I:%M:%S %p")
 sed -i '' "s|Updated: .*'|Updated: $TS'|" AWS_AIF_C01_Learning_Tool.html
